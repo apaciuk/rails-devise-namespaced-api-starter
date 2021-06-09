c35af1b33c1e2efb21ba0f5eb0f0b7cf94b81e0b
Paul Anthony McGowan <xhostcom@gmail.com>
2021-06-09 22:08:17 +0100

    First Commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  acts_as_token_authenticatable
+
+  # Note: you can include any module you want. If available,
+  # token authentication will be performed before any other
+  # Devise authentication method.
+  #
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :reviews
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
