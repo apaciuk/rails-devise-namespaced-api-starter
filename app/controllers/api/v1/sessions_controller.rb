73c92ec8ff0615418fb6b3170da06bfa0d607da7
Paul Anthony McGowan <xhostcom@gmail.com>
2021-06-10 12:32:21 +0100

    Sign Out need fix

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::SessionsController < Devise::SessionsController
-    before_action :sign_in_params, only: :create
-    before_action :load_user, only: :create
+  before_action :sign_in_params, only: :create
+  before_action :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
+
   # sign in
   def create
     if @user.valid_password?(sign_in_params[:password])
@@ -11,17 +14,33 @@ class Api::V1::SessionsController < Devise::SessionsController
     end
   end
 
+  # log out
+  def destroy
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response "Log out Successfully", true, {}, :ok
+  end
+
   private
   def sign_in_params
     params.require(:sign_in).permit(:email, :password)
   end
- 
+
   def load_user
     @user = User.find_for_database_authentication(email: sign_in_params[:email])
     if @user
       return @user
     else
       json_response "Cannot get User", false, {}, :failure
+    end
+  end
+
+  def valid_token
+    @user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+    if @user
+      return @user
+    else
+      json_response "Invalid Token", false, {}, :failure
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ApplicationRecord
   has_many :reviews
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+def generate_new_authentication_token
+    token = User.generate_unique_secure_token
+      update_attributes authentication_token: token
+  end
 end
+
